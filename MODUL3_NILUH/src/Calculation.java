c8be015a0ab67def47e1a32d07678aafab155b67
niluhpwstriii <116167670+niluhpwstriii@users.noreply.github.com>
2022-12-27 15:43:10 +0700

    Add files via upload

---
+++ MODUL3_NILUH/src/Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation implements Runnable {
+
+    Double radius;
+    Double side;
+    Double area;
+
+    Double phi = 3.14;
+
+    public void setSquare() {
+        this.Square = Square;
+    }
+}
---
+++ MODUL3_NILUH/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            
+        }
+    }
+}
